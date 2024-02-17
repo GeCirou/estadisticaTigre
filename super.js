3e4a4bb65bb56d891838ae0282b85f2e1673e06f
Ge Cirou <ca_gerardo@hotmail.com>
2024-02-17 19:17:12 -0300

    armado 1

--- super.js
+++ super.js
@@ -4,9 +4,6 @@ var totalPropina = 0;
 document.getElementById("total_propi").innerHTML = totalPropina;
 
 // LIMPIAR CASILLEROS
-function limpiarMesa1() {
-    mesa1.value = "";
-}
 function limpiarGan1() {
     gan1.value = "";
 }
@@ -17,9 +14,6 @@ function limpiarPropi1() {
     propi1.value = "";
 }
 
-function limpiarMesa2() {
-    mesa2.value = "";
-}
 function limpiarGan2() {
     gan2.value = "";
 }
@@ -30,9 +24,6 @@ function limpiarPropi2() {
     propi2.value = "";
 }
 
-function limpiarMesa3() {
-    mesa3.value = "";
-}
 function limpiarGan3() {
     gan3.value = "";
 }
@@ -43,9 +34,6 @@ function limpiarPropi3() {
     propi3.value = "";
 }
 
-function limpiarMesa4() {
-    mesa4.value = "";
-}
 function limpiarGan4() {
     gan4.value = "";
 }
@@ -56,9 +44,6 @@ function limpiarPropi4() {
     propi4.value = "";
 }
 
-function limpiarMesa5() {
-    mesa5.value = "";
-}
 function limpiarGan5() {
     gan5.value = "";
 }
@@ -69,9 +54,6 @@ function limpiarPropi5() {
     propi5.value = "";
 }
 
-function limpiarMesa6() {
-    mesa6.value = "";
-}
 function limpiarGan6() {
     gan6.value = "";
 }
@@ -82,9 +64,6 @@ function limpiarPropi6() {
     propi6.value = "";
 }
 
-function limpiarMesa7() {
-    mesa7.value = "";
-}
 function limpiarGan7() {
     gan7.value = "";
 }
@@ -95,9 +74,6 @@ function limpiarPropi7() {
     propi7.value = "";
 }
 
-function limpiarMesa8() {
-    mesa8.value = "";
-}
 function limpiarGan8() {
     gan8.value = "";
 }
@@ -108,39 +84,44 @@ function limpiarPropi8() {
     propi8.value = "";
 }
 
+// VARIABLES
+var ganancia1 = 0;
+var perdida1 =  0;
+var propina1 =  0;
+
 // CALCULO TOTAL
 function calcularTotal() {
-let ganancia1 = parseInt(document.getElementById("gan1").value);
-let perdida1 =  parseInt(document.getElementById("per1").value);
-let propina1 =  document.getElementById("propi1").value;
+    let ganancia1 = parseInt(document.getElementById("gan1").value);
+    let perdida1 =  parseInt(document.getElementById("per1").value);
+    let propina1 =  document.getElementById("propi1").value;
 
-let ganancia2 = parseInt(document.getElementById("gan2").value);
-let perdida2 = parseInt(document.getElementById("per2").value);
-let propina2 = document.getElementById("propi2").value;
+    let ganancia2 = parseInt(document.getElementById("gan2").value);
+    let perdida2 = parseInt(document.getElementById("per2").value);
+    let propina2 = document.getElementById("propi2").value;
 
-let ganancia3 = parseInt(document.getElementById("gan3").value);
-let perdida3 = parseInt(document.getElementById("per3").value);
-let propina3 = document.getElementById("propi3").value;
+    let ganancia3 = parseInt(document.getElementById("gan3").value);
+    let perdida3 = parseInt(document.getElementById("per3").value);
+    let propina3 = document.getElementById("propi3").value;
 
-let ganancia4 = parseInt(document.getElementById("gan4").value);
-let perdida4 = parseInt(document.getElementById("per4").value);
-let propina4 = document.getElementById("propi4").value;
+    let ganancia4 = parseInt(document.getElementById("gan4").value);
+    let perdida4 = parseInt(document.getElementById("per4").value);
+    let propina4 = document.getElementById("propi4").value;
 
-let ganancia5 = parseInt(document.getElementById("gan5").value);
-let perdida5 = parseInt(document.getElementById("per5").value);
-let propina5 = document.getElementById("propi5").value;
+    let ganancia5 = parseInt(document.getElementById("gan5").value);
+    let perdida5 = parseInt(document.getElementById("per5").value);
+    let propina5 = document.getElementById("propi5").value;
 
-let ganancia6 = parseInt(document.getElementById("gan6").value);
-let perdida6 = parseInt(document.getElementById("per6").value);
-let propina6 = document.getElementById("propi6").value;
+    let ganancia6 = parseInt(document.getElementById("gan6").value);
+    let perdida6 = parseInt(document.getElementById("per6").value);
+    let propina6 = document.getElementById("propi6").value;
 
-let ganancia7 = parseInt(document.getElementById("gan7").value);
-let perdida7 = parseInt(document.getElementById("per7").value);
-let propina7 = document.getElementById("propi7").value;
+    let ganancia7 = parseInt(document.getElementById("gan7").value);
+    let perdida7 = parseInt(document.getElementById("per7").value);
+    let propina7 = document.getElementById("propi7").value;
 
-let ganancia8 = parseInt(document.getElementById("gan8").value);
-let perdida8 = parseInt(document.getElementById("per8").value);
-let propina8 = document.getElementById("propi8").value;
+    let ganancia8 = parseInt(document.getElementById("gan8").value);
+    let perdida8 = parseInt(document.getElementById("per8").value);
+    let propina8 = document.getElementById("propi8").value;
 
     totalCapital = ganancia1 + ganancia2 +  ganancia3 + ganancia4 + ganancia5  + ganancia6 + ganancia7 + ganancia8 - perdida1 - perdida2 - perdida3 - perdida4 - perdida5 - perdida6 - perdida7 - perdida8;
     totalPropina = parseInt(propina1) + parseInt(propina2) + parseInt(propina3) + parseInt(propina4)  + parseInt(propina5) + parseInt(propina6) + parseInt(propina7) + parseInt(propina8);
